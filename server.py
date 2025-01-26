43759b20f68e2b2852b81dc4120ff8f99a303ea5
DavidPiwow <davidpiwowarski513@gmail.com>
2025-01-25 20:01:24 -0500

    sync

--- server.py
+++ server.py
@@ -10,30 +10,37 @@ def okay_response(msg):
     resp = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
     resp += str(msg)
     resp += "\r\n\r\n"
+
     return resp
 
 def serve_page(name):
     try:
         f = open(name, 'r')
+        f = f.read()
+        f = str(f)
     except NameError:
         print("file not found")
         return False
-    return okay_response(f.read())
+    return okay_response(f)
+
 
 
 def parse_request(buffer):
     msg_str = unquote("".join(buffer))
-    if 'color' in msg_str:
-        open_p = msg_str.find("color=(")
+    print(msg_str)
+    color_key = "color=("
+    close_tag = "close=Close"
+    if color_key in msg_str:
+        open_p = msg_str.find(color_key)
         close_p = msg_str.find(")",open_p)
-        colors = msg_str[open_p+7:close_p].split(",")
+        colors = msg_str[open_p + len(color_key) : close_p].split(",")
         if len(colors)==3:
             try:
                 display.clear((int(colors[0]), int(colors[1]), int(colors[2])))
             except ValueError:
                 return  page_get_response("alert(\"Please enter in format (R, G, B)\")")
         
-    if 'close=Close' in msg_str:
+    if close_tag in msg_str:
         return (serve_page("index.html"),1)
                 
     return serve_page("form.html")   
@@ -42,7 +49,7 @@ def main():
     listen_color = (3, 252, 198)
     wait_color = (252, 227, 3)
     disconnect_color = (158, 3, 60)
-    display.low_light = True
+    display.low_light = False
     port = 7434
     
     if (len(sys.argv) > 1):
@@ -51,22 +58,26 @@ def main():
     cur_socket = socket.socket()
     cur_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
     cur_socket.bind(('', port))
-
     
-    cur_socket.listen()
     display.clear(wait_color)
-    listening = False    
+    listening = False
+    cur_socket.listen()
+    
     while (new_conn := cur_socket.accept()):
         if not listening:
             listening = True
             display.clear(listen_color)
+        
         buffer = []
         new_socket = new_conn[0]
+        print(new_conn)
         while (segment := new_socket.recv(4096)): 
             try:
                 segment = segment.decode("utf-8")
-            finally:
-                segment = segment
+            except:
+                print(f"Error decoding {segment}")
+                new_socket.close()
+                break
             
             buffer.append(segment)
         
@@ -78,7 +89,7 @@ def main():
             new_socket.close()
             break
         else:
-            new_socket.send(okay_response(resp).encode("utf-8"))
+            new_socket.send(resp.encode("utf-8"))
         new_socket.close()
         
                         
